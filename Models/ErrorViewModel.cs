4830d83685c43f5858d38b6162e26f2eb018eb5a
Kivia Diniz Salomao <kivia.diniz18s@gmail.com>
2022-11-29 11:57:22 -0300

    atv pronta

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ATV_AV1_11_2022.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
